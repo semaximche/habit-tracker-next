07c55879227bac8e5bf684c0cd7f638d73c97a76
Maxim Cherepanov <semaximche@gmail.com>
2024-08-08 00:25:53 +0300

    no

--- src/components/profile/SideInfo.jsx
+++ src/components/profile/SideInfo.jsx
@@ -8,7 +8,7 @@ const SideInfo = ({ className }) => {
     { name: 'Манаглус', lastOnline: 'Last Online 29 hrs, 29 mins ago', badgeNumber: 1777 },
     { name: 'Vadik', lastOnline: 'Last Online 6 days ago', badgeNumber: 1234 },
     { name: 'Elie X', lastOnline: 'Last Online 40 mins ago', badgeNumber: 999 },
-    { name: 'Sorry, Im jewish', lastOnline: 'Last Online 8 hrs, 50 mins ago', badgeNumber: 228 },
+    { name: 'No', lastOnline: 'Last Online 8 hrs, 50 mins ago', badgeNumber: 228 },
     { name: '暖暖的周儿a', lastOnline: 'Last Online 15 hrs, 14 mins ago', badgeNumber: 23 }
   ];
 
