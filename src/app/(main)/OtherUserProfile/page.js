78085a82e358097cac2f276b15580abde0f99546
Maxim Cherepanov <semaximche@gmail.com>
2024-08-14 19:07:53 +0300

    fixed vercel deployment

--- src/app/(main)/OtherUserProfile/page.js
+++ src/app/(main)/OtherUserProfile/page.js
@@ -1,9 +1,12 @@
 import OtherUserProfile from '@/components/OtherUserProfile';
+import { Suspense } from 'react';
 
 export default function OtherUserProfilePage() {
     return (
         <div className="bg-gray-900 min-h-screen text-white p-5">
-            <OtherUserProfile />
+            <Suspense>
+                <OtherUserProfile />
+            </Suspense>
         </div>
     );
 }
